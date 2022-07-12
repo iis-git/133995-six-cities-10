6a522c50b3a605450918897a24a74f20df8033c8
iis-git <mr.samusenko@icloud.com>
2022-07-12 22:53:05 +0300

    feat: make task of lesson 2, add all page's components

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -6,8 +6,12 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
+const Setting = {
+  CARDS_COUNT: 3,
+};
+
 root.render(
   <React.StrictMode>
-    <App />
+    <App cardsCount={Setting.CARDS_COUNT}/>
   </React.StrictMode>,
 );
