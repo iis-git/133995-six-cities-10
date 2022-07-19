f154c7306ab47064ad693969ddce220fe399e118
iis-git <mr.samusenko@icloud.com>
2022-07-19 19:44:16 +0300

    feat: add "Page 404" component

--- project/src/pages/page404/main.tsx
+++ project/src/pages/page404/main.tsx
@@ -1,1 +1,29 @@
-export const Page404 = (): JSX.Element => <>404 Page not found :((</>;
+export const Page404 = (): JSX.Element => (
+  <>
+    <header className="header">
+      <div className="container">
+        <div className="header__wrapper">
+          <div className="header__left">
+            <a className="header__logo-link header__logo-link--active">
+              <img
+                className="header__logo"
+                src="img/logo.svg"
+                alt="6 cities logo"
+                width="81"
+                height="41"
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+    </header>
+    <main className="page__main page__main--index">
+      <section className="container">
+        <h1>404 Page not found :((</h1>{" "}
+        <a className="header__nav-links" href="/">
+          Вернуться на главную
+        </a>
+      </section>
+    </main>
+  </>
+);
