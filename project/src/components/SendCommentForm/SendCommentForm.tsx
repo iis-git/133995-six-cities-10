dccb69584a416cab650b899ff5309f9d596a0e1a
iis-git <mr.samusenko@icloud.com>
2022-08-03 20:44:25 +0300

    add: state for form data

--- project/src/components/SendCommentForm/SendCommentForm.tsx
+++ project/src/components/SendCommentForm/SendCommentForm.tsx
@@ -2,14 +2,9 @@ import React, {FC} from 'react';
 
 export const SendCommentForm: FC = () => {
   const [formData, setFormData] = React.useState({
-    rating: '3',
-    review: 'Some comment ipsum',
+    rating: null,
+    review: '',
   });
-
-  const onFormChange = (evt: React.ChangeEvent<HTMLInputElement | React.ChangeEvent<HTMLInputElement>>) => {
-    const {name, value} = evt.target;
-    setFormData({...formData, [name]: value});
-  };
 
   const updateFormState = () => {
     setFormData({
@@ -18,48 +13,52 @@ export const SendCommentForm: FC = () => {
     });
   };
 
+  const onReviewFormChange = (evt: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, value } = evt.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+
   return (
     <form className="reviews__form form" action="#" method="post">
       <label className="reviews__label form__label" htmlFor="review">Your review {formData.review}</label>
       <div className="reviews__rating-form form__rating">
-        <input className="form__rating-input visually-hidden" name="rating" value="5" id="5-stars" type="radio" onChange={onFormChange} />
+        <input className="form__rating-input visually-hidden" name="rating" value="5" id="5-stars" type="radio" onChange={onReviewFormChange}/>
         <label htmlFor="5-stars" className="reviews__rating-label form__rating-label" title="perfect">
           <svg className="form__star-image" width="37" height="33">
             <use xlinkHref="#icon-star"/>
           </svg>
         </label>
 
-        <input className="form__rating-input visually-hidden" name="rating" value="4" id="4-stars" type="radio"/>
+        <input className="form__rating-input visually-hidden" name="rating" value="4" id="4-stars" type="radio" onChange={onReviewFormChange}/>
         <label htmlFor="4-stars" className="reviews__rating-label form__rating-label" title="good">
           <svg className="form__star-image" width="37" height="33">
             <use xlinkHref="#icon-star"/>
           </svg>
         </label>
 
-        <input className="form__rating-input visually-hidden" name="rating" value="3" id="3-stars" type="radio"/>
+        <input className="form__rating-input visually-hidden" name="rating" value="3" id="3-stars" type="radio" onChange={onReviewFormChange}/>
         <label htmlFor="3-stars" className="reviews__rating-label form__rating-label" title="not bad">
           <svg className="form__star-image" width="37" height="33">
             <use xlinkHref="#icon-star"/>
           </svg>
         </label>
 
-        <input className="form__rating-input visually-hidden" name="rating" value="2" id="2-stars" type="radio"/>
+        <input className="form__rating-input visually-hidden" name="rating" value="2" id="2-stars" type="radio" onChange={onReviewFormChange}/>
         <label htmlFor="2-stars" className="reviews__rating-label form__rating-label" title="badly">
           <svg className="form__star-image" width="37" height="33">
             <use xlinkHref="#icon-star"/>
           </svg>
         </label>
 
-        <input className="form__rating-input visually-hidden" name="rating" value="1" id="1-star" type="radio"/>
+        <input className="form__rating-input visually-hidden" name="rating" value="1" id="1-star" type="radio" onChange={onReviewFormChange}/>
         <label htmlFor="1-star" className="reviews__rating-label form__rating-label" title="terribly">
           <svg className="form__star-image" width="37" height="33">
             <use xlinkHref="#icon-star"/>
           </svg>
         </label>
       </div>
-      <textarea className="reviews__textarea form__textarea" id="review" name="review"
-        placeholder="Tell how was your stay, what you like and what can be improved" onChange={onFormChange} value={formData.review}
-      />
+      <textarea className="reviews__textarea form__textarea" id="review" name="review" placeholder="Tell how was your stay, what you like and what can be improved" onChange={onReviewFormChange}/>
       <div className="reviews__button-wrapper">
         <p className="reviews__help">
           To submit review please make sure to set <span className="reviews__star">rating</span> and describe your stay
